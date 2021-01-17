895e1dbd1c842be164cc163f0a28c83130212573
Spinkoo <38700565+Spinkoo@users.noreply.github.com>
2021-01-17 20:12:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from tp import *
 pts_path="300w_train_landmarks.txt"
 imgs_path="300w_train_images.txt"
 	
-pts_test="lfpw_testset_landmarks.txt"
-imgs_test="lfpw_testset.txt"
+pts_test="helen_testset_landmarks.txt"
+imgs_test="helen_testset.txt"
 m=Model()
 #display cropped images
 #test_display_cropped(imgs_path,pts_path)
